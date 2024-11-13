bd2dfd4501218e27e3a6e9add7b00c897fa3fd5c
Bela <belacaste3@gmail.com>
2024-11-13 00:27:35 -0500

    nav finished

--- src/app/layout/header/header-home/header-home.component.ts
+++ src/app/layout/header/header-home/header-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../../sidebar/sidebar.component';
+import { RouterEvent, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, RouterLink],
   templateUrl: './header-home.component.html',
   styleUrl: './header-home.component.css'
 })
